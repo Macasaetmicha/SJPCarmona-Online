dc9e7f162ee31bb19ef4feb22e967970a23dacd5
michaela_m <96478630+Macasaetmicha@users.noreply.github.com>
2025-05-21 01:49:37 +0800

    Update config.py

--- website/config/config.py
+++ website/config/config.py
@@ -1,5 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
