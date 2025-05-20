6b5fcdd545a7c039f4c0f7e89adf92b768ff97ec
michaela_m <96478630+Macasaetmicha@users.noreply.github.com>
2025-05-21 02:00:37 +0800

    Update config.py

--- website/config/config.py
+++ website/config/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    DB_HOST = os.getenv('DB_HOST')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('MYSQLHOST')
+    DB_NAME = os.getenv('MYSQL_DATABASE')
+    DB_USER = os.getenv('MYSQLUSER')
+    DB_PASSWORD = os.getenv('MYSQLPASSWORD')
