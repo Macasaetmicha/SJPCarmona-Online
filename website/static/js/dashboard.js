9efd6d134af087fd60a0e88b78376e756a576215
michaela_m <96478630+Macasaetmicha@users.noreply.github.com>
2025-05-24 13:28:39 +0800

    Update dashboard.js

--- website/static/js/dashboard.js
+++ website/static/js/dashboard.js
@@ -289,7 +289,6 @@ document.addEventListener('DOMContentLoaded', function () {
             const modal = new bootstrap.Modal(document.getElementById('eventDetailModal'));
             modal.show();
         }
-
     });
 
     calendar.render();
