874828834b3c5d37d7a32277667da964a67d7184
michaela_m <96478630+Macasaetmicha@users.noreply.github.com>
2025-05-27 13:37:33 +0800

    Create request_client_page.js

---
+++ website/static/js/request_client_page.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+ $(document).on('click', '.update-status-btn', function () {
+        const id = $(this).data('id');
+        const status = $(this).data('status');
+        const page = $(this).data('page'); 
+
+        console.log("UPDATING THE STATUS")
+    
+        if (confirm('Are you sure you want to change the status?')) {
+            $.ajax({
+                url: /edit-clientReq/${page}, 
+                type: 'POST',
+                data: { id, status },
+                success: function (response) {
+                    alert('Status updated successfully!');
+                    // Optionally refresh table or UI here
+                },
+                error: function () {
+                    alert('Failed to update status.');
+                }
+            });
+        }
+    });
+});
